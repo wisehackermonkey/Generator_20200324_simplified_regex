54a3ccff34ffffda561adcf37fc660cf843716f6
wisemonkey <oranbusiness@gmail.com>
2020-03-24 08:12:39 -0700

    [add] init project file, and licencse update

---
+++ Generator.py
@@ -0,0 +1,18 @@
+#Simplified regex text generator 
+#20200324
+#by oran collins
+
+
+
+
+
+
+def generator_letter_range(start="", end=""):
+    return "works"
+
+
+
+
+
+if __name__ == "__main__":
+    print(generator_letter_range(start="a",end="z"))
